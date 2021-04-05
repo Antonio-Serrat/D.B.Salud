ba21867edf22b9ed777498c95211224c73399157
Tonisrt <4antoniogabriel20@gmail.com>
2021-04-05 19:10:38 -0300

    upload

--- src/main/java/com/AppClinics/controller/TherapistController.java
+++ src/main/java/com/AppClinics/controller/TherapistController.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,7 +50,7 @@ public class TherapistController {
 	}
 
 	@RequestMapping(value = "/newPatient", method = { RequestMethod.POST, RequestMethod.PUT })
-	public String savePatient(@RequestParam(value = "name") String name,
+	public String savePatient(@ModelAttribute Patient person, @RequestParam(value = "name") String name,
 			@RequestParam(value = "surname") String surname, @RequestParam(value = "age") Integer age,
 			@RequestParam(value = "phone") Integer phone, @RequestParam(value = "email") String email,
 			@RequestParam(value = "birthdate") String birthdate,
